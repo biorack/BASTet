3036993738733b488968205b12bba29a7fbc915f
Oliver Ruebel <oruebel@lbl.gov>
2014-01-24 22:34:20 +0000

    Cleaned up hilbert curve example
    git-svn-id: https://code.lbl.gov/svn/openmsi-tk/trunk@48 770f0362-5d60-4208-80c3-892ab42ae3ed

--- omsi/examples/hilbert_spectrum.py
+++ omsi/examples/hilbert_spectrum.py
@@ -18,11 +18,45 @@ def hilbert_curve(order=2):
         yre = np.r_[x, order/2+y, order/2+y, order/2-1-x]
         return xre, yre
 
+def plot_hilbert_image(x,y,val,order) : 
+    """Plot image with pixels colored according to val and layed out 
+       based on the hilbert coordinates given by x and y.
+    """
+    #Create hilbert image
+    img = np.zeros( shape=(order,order) )
+    img[x,y] = val
 
-order = 1024
-x,y = hilbert_curve(order=order)
-val=np.arange(len(x))
-img = np.zeros( shape=(order,order) )
-img[x,y] = val
-plt.imshow(img)
-plt.show()
+    #Plot the hilber image
+    imgplot = plt.imshow(img)
+    imgplot.set_interpolation('nearest')
+    plt.show()
+
+    return imgplot
+
+def plot_hilbert_curve(x,y) :
+    """Plot the hilbert curve specified by the coordinate arrays x,y."""
+    plt.plot(x,y)
+    plt.show()
+
+def main(argv=None):
+    '''Then main function'''
+
+    #Read input arguments
+    import sys
+    from sys import argv,exit
+    if argv is None:
+        argv = sys.argv
+    order = 128
+    if len(argv)>1 : 
+        order = int(argv[1])
+
+    #Compute the hilbert curve
+    x,y = hilbert_curve(order=order)
+    val=np.arange(len(x))
+    
+    plot_hilbert_image(x,y,val,order)
+    plot_hilbert_curve(x,y)
+
+if __name__ == "__main__":
+    main()
+
