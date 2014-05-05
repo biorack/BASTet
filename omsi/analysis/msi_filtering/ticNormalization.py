b059d0a6582433e108b82b6dfc3f4d8ba015518a
Benjamin P. Bowen <bpbowen@lbl.gov>
2014-05-05 20:46:06 +0000

    with tic normalization
    git-svn-id: https://code.lbl.gov/svn/openmsi-tk/trunk@189 770f0362-5d60-4208-80c3-892ab42ae3ed

--- omsi/analysis/msi_filtering/ticNormalization.py
+++ omsi/analysis/msi_filtering/ticNormalization.py
@@ -54,9 +54,11 @@ class omsi_ticNorm(omsi_analysis_base) :
             self['maxCount']=0
         if not self['mzTol'] :
             self['mzTol']=0.1
-        if not self['infIons'] :
+
+        try:
+            ionList = self['infIons']
             useTIC = 1
-        else:
+        except:
             useTIC = 0
         # if not self['objectiveDim'] :
         #     self['objectiveDim']=self.dimension_index['imageDim']
