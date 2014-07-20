9dd8331fce5a7bfcc0db9b79b80be33578acb1da
Oliver Ruebel <oruebel@lbl.gov>
2014-07-20 20:20:34 +0000

    Updated init.py for omsi.dataformat to make sure that the mzml format reader is only imported when pyteomics is available, making pyteomics optional
    git-svn-id: https://code.lbl.gov/svn/openmsi-tk/trunk@211 770f0362-5d60-4208-80c3-892ab42ae3ed

--- omsi/dataformat/__init__.py
+++ omsi/dataformat/__init__.py
@@ -1,1 +1,10 @@
-__all__ = ["img_file", "bruckerflex_file", "mzml_file", "omsi_file", "omsi_format", "file_reader_base"]
+def mzml_available():
+    try:
+        import pyteomics
+        return True
+    except:
+        return False
+
+__all__ = ["img_file", "bruckerflex_file", "omsi_file", "omsi_format", "file_reader_base"]
+if mzml_available():
+    __all__.append("mzml_file")
