849628946846042c161b50c4063cfc6ce8e0b26b
Oliver Ruebel <oruebel@lbl.gov>
2015-02-10 23:42:50 +0000

    Updated omsi_spectrum_layout to improve coding style and ensure compliance with other changes
    git-svn-id: https://code.lbl.gov/svn/openmsi-tk/trunk@277 770f0362-5d60-4208-80c3-892ab42ae3ed

--- omsi/shared/omsi_spectrum_layout.py
+++ omsi/shared/omsi_spectrum_layout.py
@@ -4,16 +4,17 @@ import math
 
 
 def plot_2d_spectrum_as_image(hilbert_intensities, show_plot=False, show_axis=False):
-    """Plot image with pixels colored according to hilbert_intensities.
+    """
+    Plot image with pixels colored according to hilbert_intensities.
 
-       :param hilbert_intensities: 2D numpy array with the intensity values for the spectrum.
-       :type hilbert_intensities: 2D numpy array.
-       :param show_plot: Show the generated plot in a window.
-       :type show_plot: Boolean
-       :param show_axis: Show x,y axis for the plot. Default is False.
-       :type show_axis: Boolean
+    :param hilbert_intensities: 2D numpy array with the intensity values for the spectrum.
+    :type hilbert_intensities: 2D numpy array.
+    :param show_plot: Show the generated plot in a window.
+    :type show_plot: Boolean
+    :param show_axis: Show x,y axis for the plot. Default is False.
+    :type show_axis: Boolean
 
-       :returns: matplotlib image plot or None in case that the plotting failed. 
+    :returns: matplotlib image plot or None in case that the plotting failed.
 
     """
     try:
@@ -23,9 +24,9 @@ def plot_2d_spectrum_as_image(hilbert_intensities, show_plot=False, show_axis=Fa
         import matplotlib.pyplot as plt
         if not show_plot:
             plt.ioff()
-    except:
+    except ImportError:
         return None
-    # Plot the hilber image
+    # Plot the hilbert image
     plt.clf()
     if show_axis:
         imgplot = plt.imshow(np.log(hilbert_intensities + 1))
@@ -44,26 +45,27 @@ def plot_2d_spectrum_as_image(hilbert_intensities, show_plot=False, show_axis=Fa
         ax.imshow(np.log(hilbert_intensities + 1),
                   aspect='normal', interpolation='nearest')
         if show_plot:
-            plot.show()
+            plt.show()
         return ax, fig
 
 
 def compute_hilbert_spectrum(original_coords, original_intensities, left=0, right=0):
-    """Given a 1D spectrum, interpolate the spectrum onto the closest 2D hilbert curve.
+    """
+    Given a 1D spectrum, interpolate the spectrum onto the closest 2D hilbert curve.
 
-       :param original_coords: The original coordinate values (m/z). Values must be increasing.
-       :type original_coords: 1D numpy array in increasing order.
-       :param original_intensities: The original intensity values. Same length as original_coords.
-       :type original_intensities: 1D numpy array of same length as original_coords
-       :param left: Optional. Value to be used for padding data at the lower bound during interpolation
-       :type: float
-       :param right: Optional. Value to be used for padding data at the upper bound during interpolation
-       :type: float
+    :param original_coords: The original coordinate values (m/z). Values must be increasing.
+    :type original_coords: 1D numpy array in increasing order.
+    :param original_intensities: The original intensity values. Same length as original_coords.
+    :type original_intensities: 1D numpy array of same length as original_coords
+    :param left: Optional. Value to be used for padding data at the lower bound during interpolation
+    :type: float
+    :param right: Optional. Value to be used for padding data at the upper bound during interpolation
+    :type: float
 
-       :returns: 2D numpy array with the coordinate (m/z) values for the hilbert spectrum and separate
-                 2D numpy array for the interpolated intensity values.
+    :returns: 2D numpy array with the coordinate (m/z) values for the hilbert spectrum and separate
+             2D numpy array for the interpolated intensity values.
 
-       :raises: ValueError If original_coords and original_intensities have different length.
+    :raises: ValueError If original_coords and original_intensities have different length.
     """
     # Check if the inputs are Ok
     if original_coords.shape[0] != original_intensities.shape[0]:
@@ -72,7 +74,7 @@ def compute_hilbert_spectrum(original_coords, original_intensities, left=0, righ
 
     # Compute the length of the two curves
     originallength = original_coords.shape[0]
-    #hilbertlength =  math.pow(2, math.ceil(math.log(originallength)/math.log(2)))
+    # hilbertlength =  math.pow(2, math.ceil(math.log(originallength)/math.log(2)))
     hilbertorder = math.sqrt(originallength)
     # find the closest power of 2
     hilbertorder = math.pow(2, math.ceil(math.log(hilbertorder) / math.log(2)))
@@ -83,7 +85,7 @@ def compute_hilbert_spectrum(original_coords, original_intensities, left=0, righ
 
     # Compute the hilbert curve
     hilbertmz = ((np.arange(hilbertlength, dtype=np.dtype('float'))
-                 / float(hilbertlength - 1)) * rangemz) + minmz
+                  / float(hilbertlength - 1)) * rangemz) + minmz
     hilbertx, hilberty = hilbert_curve(order=hilbertorder)
 
     # Interpolate the intensity values
@@ -106,17 +108,18 @@ def compute_hilbert_spectrum(original_coords, original_intensities, left=0, righ
 
 
 def reinterpolate_spectrum(coords, original_coords, original_intensitities, left=0, right=0):
-    """Given a 1D spectrum, interpolate the spectrum onto a new axis.
+    """
+    Given a 1D spectrum, interpolate the spectrum onto a new axis.
 
-       :param coords: The coordinate values (m/z) for which intensities should be computed.
-       :param original_coords: The original coordinate values (m/z). Values must be increasing.
-       :param original_intensitities: The original intensity values. Same length as original_coords.
-       :param left: Optional. Value to be used if coords < orignal_coords
-       :param right: Optional. Value to be used if coords > orignal_coords
+    :param coords: The coordinate values (m/z) for which intensities should be computed.
+    :param original_coords: The original coordinate values (m/z). Values must be increasing.
+    :param original_intensitities: The original intensity values. Same length as original_coords.
+    :param left: Optional. Value to be used if coords < orignal_coords
+    :param right: Optional. Value to be used if coords > orignal_coords
 
-       :returns: y : {float, ndarray} The interpolated values, same shape as coords.
+    :returns: y : {float, ndarray} The interpolated values, same shape as coords.
 
-       :raises: ValueError If original_coords and original_intensities have different length.
+    :raises: ValueError If original_coords and original_intensities have different length.
     """
     return np.interp(x=coords,
                      xp=original_coords,
@@ -126,14 +129,15 @@ def reinterpolate_spectrum(coords, original_coords, original_intensitities, left
 
 
 def hilbert_curve(order=2):
-    """Compute  a 2D hilbert curve.
+    """
+    Compute  a 2D hilbert curve.
 
-       :param order: The order of the hilber curve. This is the length of the sides of the square, i.e.,
-                     the number of points in x and y.
-       :type order: Integer that defines a power of 2 (>=2)
+    :param order: The order of the hilber curve. This is the length of the sides of the square, i.e.,
+                 the number of points in x and y.
+    :type order: Integer that defines a power of 2 (>=2)
 
-       :returns: Returns two numpy arrays of integers x,y, indicating the locations of the
-                 vertices of the hilbert curve.
+    :returns: Returns two numpy arrays of integers x,y, indicating the locations of the
+             vertices of the hilbert curve.
 
     """
     if order == 2:
