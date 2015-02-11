c0ab8c9cfc1c4e875f55cb1221638b3f889d6609
Oliver Ruebel <oruebel@lbl.gov>
2015-02-11 00:22:17 +0000

    Added omsi_analysis_base amd omsi_analysis_generic to the list of default imports for omsi.analysis
    git-svn-id: https://code.lbl.gov/svn/openmsi-tk/trunk@289 770f0362-5d60-4208-80c3-892ab42ae3ed

--- omsi/analysis/__init__.py
+++ omsi/analysis/__init__.py
@@ -1,12 +1,22 @@
-from findpeaks import *
-from multivariate_stats import *
-from msi_filtering import *
-from omsi_analysis_base import *
-from omsi_analysis_data import *
-from omsi_analysis_generic import *
-from omsi_viewer_helper import *
+"""
+OpenMSI toolkit analysis package defining the main analysis API as well as
+specific MSI analysis algorithms.
+"""
+
+from omsi.analysis.findpeaks import *
+from omsi.analysis.multivariate_stats import *
+from omsi.analysis.msi_filtering import *
+from omsi.analysis.omsi_analysis_base import *
+from omsi.analysis.omsi_analysis_data import *
+from omsi.analysis.omsi_analysis_generic import *
+import omsi.analysis.findpeaks
+import omsi.analysis.multivariate_stats
+import omsi.analysis.msi_filtering
+# from omsi_viewer_helper import *
 __all__ = ["findpeaks",
            "multivariate_stats",
            "msi_filtering",
-           "omsi_analysis_data"] + findpeaks.all__ + multivariate_stats.all__ + msi_filtering.all__
+           "omsi_analysis_data",
+           "omsi_analysis_base",
+           "omsi_analysis_generic"] + findpeaks.all__ + multivariate_stats.all__ + msi_filtering.all__
 
