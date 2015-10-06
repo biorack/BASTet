454627b4c55f71779aaa306b3c50d71d4f48219d
Oliver Ruebel <oruebel@lbl.gov>
2015-10-06 00:56:13 +0000

    Refactored code to distinguish between executors of workflows and drivers. Executors run the workfows and drivers set them up
    git-svn-id: https://code.lbl.gov/svn/openmsi-tk/trunk@505 770f0362-5d60-4208-80c3-892ab42ae3ed

--- omsi/workflow/driver/__init__.py
+++ omsi/workflow/driver/__init__.py
@@ -1,6 +1,6 @@
 """
-Module with drivers for analyses and workflows. Drivers are intented to control the creation
-and initation of the execution of workflows. The execution of workflows itself is typically
-performed by a workflow driver. .
+Module with drivers for analyses and workflows. Drivers are intented to control the set up
+---i.e., creation and initialization---of workflows. The execution of workflows itself is typically
+performed by a workflow executor.
 """
 __author__ = 'oruebel'
