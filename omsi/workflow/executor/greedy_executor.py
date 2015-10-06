0ef4877cd3d3c484f086ee33fe4d5f893f7460e9
Oliver Ruebel <oruebel@lbl.gov>
2015-10-06 22:28:56 +0000

    Updated the greedy workflow executor. Added mpi_root and mpi_comm parameters and updated logging for parallel. Added reduce_memory_usage option using the new analysis.clear_and_restore feature to save analyses to temporary files and use the data out-of-core
    git-svn-id: https://code.lbl.gov/svn/openmsi-tk/trunk@516 770f0362-5d60-4208-80c3-892ab42ae3ed

--- omsi/workflow/executor/greedy_executor.py
+++ omsi/workflow/executor/greedy_executor.py
@@ -4,10 +4,19 @@ Module used to help with the execution of complex analyses workflows
 from omsi.workflow.base import workflow_executor_base
 from omsi.shared.run_info_data import run_info_dict
 from omsi.shared.log import log_helper
+import omsi.shared.mpi_helper as mpi_helper
 
 class greedy_executor(workflow_executor_base):
     """
     Execute a set of analysis objects and their dependencies
+
+    :ivar run_info: The runtime information dictionary for the overall workflow
+    :ivar mpi_comm: The MPI communicator to be used when running in parallel
+    :ivar mpi_root: The MPI root rank when running in parallel
+    :ivar reduce_memory_usage: Boolean indicating whether we should reduce memory usage by pushing analysis
+        data to file after an analysis has been completed. This reduces the amount of data we keep in memory
+        but results in additional overhead for I/O and temporary disk storage.
+
     """
     def __init__(self, analysis_objects=None):
         """
@@ -17,33 +26,39 @@ class greedy_executor(workflow_executor_base):
         """
         super(greedy_executor, self).__init__(analysis_objects)
         self.run_info = run_info_dict()
+        self.mpi_comm = mpi_helper.get_comm_world()
+        self.mpi_root = 0
+        self.reduce_memory_usage = True
 
     def main(self):
         """Execute the analysis workflow"""
         if len(self.get_analyses()) == 0:
-            log_helper.info(__name__, "The workflow is empty")
+            log_helper.info(__name__, "The workflow is empty", root=self.mpi_root, comm=self.mpi_comm)
             return
 
         # Add all dependencies to the workflow
-        log_helper.debug(__name__, "Executing the workflow")
-        log_helper.info(__name__, "Adding all dependencies")
+        log_helper.debug(__name__, "Executing the workflow", root=self.mpi_root, comm=self.mpi_comm)
+        log_helper.info(__name__, "Adding all dependencies", root=self.mpi_root, comm=self.mpi_comm)
         self.add_analysis_dependencies()
 
         # Record the runtime information
-        log_helper.debug(__name__, "Recording runtime information")
+        log_helper.debug(__name__, "Recording runtime information", root=self.mpi_root, comm=self.mpi_comm)
         self.run_info.clear()
         self.run_info.record_preexecute()
 
         # Execute the workflow in a greedy fashion (i.e., execute whichever analysis is ready and has not be run yet)
-        log_helper.debug(__name__, "Running the analysis workflow")
+        log_helper.debug(__name__, "Running the analysis workflow", root=self.mpi_root, comm=self.mpi_comm)
         all_analyses = self.get_analyses()
         iterations = 0
         while True:
             # Run all analyses that are ready
             for analysis in all_analyses:
                 if analysis.update_analysis and len(analysis.check_ready_to_execute()) == 0:
-                    log_helper.debug(__name__, "Execute analysis: " + str(analysis))
+                    log_helper.debug(__name__, "Execute analysis: " + str(analysis),
+                                     root=self.mpi_root, comm=self.mpi_comm)
                     analysis.execute()
+                    if self.reduce_memory_usage:
+                        analysis.clear_and_restore()
             # Check if there is any other tasks that we need to execte now
             num_tasks = 0
             num_tasks_ready = 0
@@ -53,14 +68,15 @@ class greedy_executor(workflow_executor_base):
                     if len(analysis.check_ready_to_execute()) == 0:
                         num_tasks_ready += 1
             if num_tasks == 0:
-                log_helper.info(__name__, "Completed executing the workflow.")
+                log_helper.info(__name__, "Completed executing the workflow.", root=self.mpi_root, comm=self.mpi_comm)
                 break
             if num_tasks > 0 and num_tasks_ready == 0:
                 log_helper.warning(__name__, "Workflow could not be fully executed. " + str(num_tasks) +
-                                   " remain in the queue but cannot be completed due to unresolved dependencies.")
+                                   " remain in the queue but cannot be completed due to unresolved dependencies.",
+                                   root=self.mpi_root, comm=self.mpi_comm)
             iterations += 1
 
-        log_helper.log_var(__name__, iterations=iterations, level='DEBUG')
+        log_helper.log_var(__name__, iterations=iterations, level='DEBUG', root=self.mpi_root, comm=self.mpi_comm)
 
         # Record the runtime information after we are done with the workflow
         self.run_info.record_postexecute()
