0919c0d23f42f20ec2354d27b7e90b670b2f6530
Oliver Ruebel <oruebel@lbl.gov>
2015-10-08 07:06:33 +0000

    Correct intendation error in run_analysis
    git-svn-id: https://code.lbl.gov/svn/openmsi-tk/trunk@526 770f0362-5d60-4208-80c3-892ab42ae3ed

--- omsi/tools/run_analysis.py
+++ omsi/tools/run_analysis.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
 
     # Create a command-line driver and call main to run the analysis
     cl_analysis_driver(analysis_class=None,
-                   add_analysis_class_arg=True,
-                   add_output_arg=True,
-                   add_profile_arg=True,
-                   add_mem_profile_arg=True).main()
+                       add_analysis_class_arg=True,
+                       add_output_arg=True,
+                       add_log_level_arg=True,
+                       add_profile_arg=True,
+                       add_mem_profile_arg=True).main()
