d831e92eeb3304c0dbeaf5bf2ca3417da33ef821
Oliver Ruebel <oruebel@lbl.gov>
2015-12-18 19:58:24 +0000

    Some updates to setup.py
    git-svn-id: https://code.lbl.gov/svn/openmsi-tk/trunk@592 770f0362-5d60-4208-80c3-892ab42ae3ed

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from distutils.core import setup
 
 setup(name='omsi',
       version='0.1',
-      description='OpenMSI Toolkit',
-      author='Oliver Ruebel and Ben Bowen',
-      author_email=' ',
+      description='Berkeley Analysis and Storage Toolkit (BASTet)',
+      author='Oliver Ruebel and Benjamin P. Bowen',
+      author_email='oruebel@lbl.gov',
       url='http://openmsi.nersc.gov',
       packages=['omsi' , 'omsi/analysis' , 'omsi/dataformat' , 'omsi/tools' , 'omsi/viewer'],
-      requires=['numpy' , 'h5py']
+      requires=['numpy' , 'h5py'],
+      extras_require=['pillow', 'django', 'psutil', 'pyteomics', 'mpi4py', 'memory_profiler', 'lxml' ],
+      license="See licence.txt.",
+      keywords="storage MSI analysis workflow provenance"
      )
