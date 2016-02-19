ed608b5f89815c642166c2718f8d5395847999a2
Oliver Ruebel <oruebel@lbl.gov>
2016-02-19 20:36:59 +0000

    Moved kmeans analysis from experimental to main and updated the cluster analyses to work more generally
    git-svn-id: https://code.lbl.gov/svn/openmsi-tk/trunk@600 770f0362-5d60-4208-80c3-892ab42ae3ed

--- omsi/analysis/multivariate_stats/__init__.py
+++ omsi/analysis/multivariate_stats/__init__.py
@@ -1,6 +1,18 @@
 """
 Multivariate statistics analysis
 """
-all__ = ["omsi_nmf", "omsi_cx"]
+
+
+def kmeans_available():
+    try:
+        from scipy.cluster.vq import kmeans, vq
+        return True
+    except ImportError:
+        return False
+
+__all__ = ["omsi_nmf", "omsi_cx"]
 from omsi_nmf import *
 from omsi_cx import *
+if kmeans_available():
+    __all__ += ["omsi_kmeans"]
+from omsi_kmeans import *
