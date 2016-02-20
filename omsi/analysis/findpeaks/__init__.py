fd9b5782b66d6e1e259352fb6b875079ec15e5be
Oliver Ruebel <oruebel@lbl.gov>
2016-02-20 02:42:03 +0000

    Bug fix for __init__ files, renamed __all to __all__
    git-svn-id: https://code.lbl.gov/svn/openmsi-tk/trunk@604 770f0362-5d60-4208-80c3-892ab42ae3ed

--- omsi/analysis/findpeaks/__init__.py
+++ omsi/analysis/findpeaks/__init__.py
@@ -2,6 +2,6 @@
 Package of peak-finding related analysis modules.
 """
 
-all__ = ["omsi_findpeaks_global", "omsi_findpeaks_local"]
+__all__ = ["omsi_findpeaks_global", "omsi_findpeaks_local"]
 from omsi.analysis.findpeaks.omsi_findpeaks_global import omsi_findpeaks_global
 from omsi.analysis.findpeaks.omsi_findpeaks_local import omsi_findpeaks_local
